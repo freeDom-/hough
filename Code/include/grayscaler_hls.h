77262adc142cf94e000003af8f2df13f56763b30
freeDom- <dominik.weinrich@gmx.de>
2018-06-26 16:20:50 +0200

    small structural changes

---
+++ Code/include/grayscaler_hls.h
@@ -0,0 +1,6 @@
+#define WIDTH 400
+#define HEIGHT 400
+#define SIZE WIDTH * HEIGHT
+#define INPUT_IMAGE "/home/dom/Bachelorarbeit/img/gen/data.txt"
+
+void grayscaler(uint32_t input[SIZE], uint8_t output[SIZE]);
